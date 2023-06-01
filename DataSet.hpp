d1cdff6409c7b1d5de0e20c51df7b0bb04b86a1e
AlejandroAVILAd <135281323+AlejandroAVILAd@users.noreply.github.com>
2023-06-01 16:33:04 -0600

    Add files via upload

---
+++ DataSet.hpp
@@ -0,0 +1,81 @@
+#ifndef DATA_SET
+#define DATA_SET
+#include <vector>
+
+class DataSet
+{
+private:
+
+    std::vector<float> x;
+    std::vector<float> y;
+    std::vector<char> types;
+
+public:
+    DataSet();
+    std::vector<float> getX();
+    std::vector<float> getY();
+    std::vector<char> getTypes();
+};
+
+DataSet::DataSet()
+{
+    x = {25, // Edades
+         35,
+         45,
+         20,
+         35,
+         52,
+         23,
+         40,
+         60,
+         48,
+         33,
+         48};
+
+    y = {
+        40000,  // Prestamos
+        60000,
+        80000,
+        20000,
+        120000,
+        18000,
+        95000,
+        62000,
+        1000000,
+        2200000,
+        150000,
+        142000,
+    };
+
+    types = {
+        'N',
+        'N',
+        'N',
+        'N',
+        'N',
+        'N',
+        'Y',
+        'Y',
+        'Y',
+        'Y',
+        'Y',
+        'Y',
+    };
+}
+
+std::vector<float> DataSet::getX()
+{
+    return x;
+}
+
+std::vector<float> DataSet::getY()
+{
+    return y;
+}
+
+std::vector<char> DataSet::getTypes()
+{
+    return types;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include "DataSet.hpp"
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+
+using namespace std;
+
+bool compare(const pair<float, char> &i, const pair<float, char> &j);
+vector<pair<float, char>> mideDistancia(vector<float> x, vector<float> y, vector<char> types, int age, int loan, int size);
+void muestraTipo(vector<pair<float, char>> distance, int k, int age, int loan);
+
+int main()
+{
+    //Cuántos vecinos más cercanos
+    const int K = 5;
+
+    // Lista de medidas
+    vector<pair<float, char>> medidas;
+
+    // Nuevo dato : edad
+    const int NEW_AGE = 48;
+
+    const int LOAN = 142000; // Préstamo
+
+    DataSet data;
+
+    vector<float> x;
+    vector<float> y;
+    vector<char> tipos;
+
+    x = data.getX();
+    y = data.getY();
+    tipos = data.getTypes();
+
+    cout<<endl;
+
+    medidas = mideDistancia(x, y, tipos, NEW_AGE, LOAN, x.size());
+    muestraTipo(medidas, K, NEW_AGE, LOAN);
+
+    return EXIT_SUCCESS;
+}
+
+
+bool compare(const pair<float, char> &i, const pair<float, char> &j)
+{
+    return i.first < j.first;
+}
+
+vector<pair<float, char>> mideDistancia(vector<float> x, vector<float> y, vector<char> types, int age, int loan, int size)
+{
+    vector<pair<float, char>> medidas;
+    for (int c = 0; c < size; c++)
+    {
+        const float distance = sqrt(pow(x[c] - age, 2) + pow(y[c] - loan, 2));
+        pair<float, char> data;
+        data.second = types[c];
+        data.first = distance;
+        medidas.insert(medidas.end(), data);
+    }
+
+    std::sort(medidas.begin(), medidas.end(), compare);
+
+    return medidas;
+}
+
+void muestraTipo(vector<pair<float, char>> distance, int k, int age, int loan)
+{
+    cout << endl;
+    int defaultN = 0;
+    int defaultY = 0;
+
+    for (int c = 0; c < k; c++)
+    {
+        cout << " " << c + 1 << " Dis: " << distance[c].first << " Type: " << distance[c].second << endl;
+        if (distance[c].second == 'N')
+            defaultN++;
+        else
+            defaultY++;
+    }
+
+    if (defaultN < defaultY)
+        cout << "Es un SI" << endl;
+    else
+        cout << "Es un NO" << endl;
+}
+
